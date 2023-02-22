dc407889decb98fa8b3f7832edcfd14c07ba79f1
gitakshay1 <akshaypatil309p8@gmail.com>
2023-02-22 21:42:56 +0530

    [Akshay] add program to create default values in list

---
+++ ProductReviewMangement/LinqOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewMangement
+{
+    public class LinqOperations
+    {
+        public void ProductPrint(List<ProductReview> listProductReview)
+        {
+            Console.WriteLine("Printing product reviewlist");
+            //Print products
+            foreach (var productReview in listProductReview)
+            {
+                Console.WriteLine($"ProductID: {productReview.ProductId},UserId: {productReview.UserId}Rating: {productReview.Rating},Review: {productReview.Review}");
+            }
+        }
+    }
+}
---
+++ ProductReviewMangement/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace ProductReviewMangement
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            List<ProductReview> listProductReview = new List<ProductReview>()
+            {
+               new ProductReview() { ProductId = 1, UserId = 1, Rating = 5, Review = "Good", isLike = true },
+            new ProductReview() { ProductId = 2, UserId = 1, Rating = 4, Review = "Nice", isLike = true },
+            new ProductReview() { ProductId = 3, UserId = 1, Rating = 5, Review = "Good", isLike = true },
+            new ProductReview() { ProductId = 4, UserId = 2, Rating = 5, Review = "Good", isLike = true },
+            new ProductReview() { ProductId = 5, UserId = 2, Rating = 3, Review = "NotBad", isLike = false },
+            new ProductReview() { ProductId = 6, UserId = 3, Rating = 5, Review = "Good", isLike = true },
+            new ProductReview() { ProductId = 7, UserId = 4, Rating = 5, Review = "Good", isLike = true },
+            new ProductReview() { ProductId = 8, UserId = 4, Rating = 3, Review = "NotBad", isLike = false },
+            new ProductReview() { ProductId = 9, UserId = 4, Rating = 5, Review = "Good", isLike = true },
+            new ProductReview() { ProductId = 10, UserId = 4, Rating = 5, Review = "Good", isLike = true },
+            new ProductReview() { ProductId = 11, UserId = 5, Rating = 2, Review = "Bad", isLike = false },
+             new ProductReview() { ProductId = 12, UserId = 5, Rating = 5, Review = "Good", isLike = true },
+            new ProductReview() { ProductId = 13, UserId = 5, Rating = 5, Review = "Good", isLike = true },
+            new ProductReview() { ProductId = 14, UserId = 5, Rating = 2, Review = "Bad", isLike = false },
+            new ProductReview() { ProductId = 15, UserId = 6, Rating = 5, Review = "Good", isLike = true },
+            new ProductReview() { ProductId = 16, UserId = 6, Rating = 5, Review = "Good", isLike = true },
+            new ProductReview() { ProductId = 17, UserId = 6, Rating = 2, Review = "Bad", isLike = false },
+            new ProductReview() { ProductId = 18, UserId = 6, Rating = 5, Review = "Good", isLike = true },
+            new ProductReview() { ProductId = 19, UserId = 7, Rating = 3, Review = "NotBad", isLike = false },
+            new ProductReview() { ProductId = 20, UserId = 7, Rating = 5, Review = "Good", isLike = true },
+            new ProductReview() { ProductId = 21, UserId = 7, Rating = 4, Review = "Nice", isLike = true },
+            };
+            LinqOperations linqOperations = new LinqOperations();
+            linqOperations.ProductPrint(listProductReview);
+        }
+    }
+}
